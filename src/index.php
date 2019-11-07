f578eab07c5f7ade609a709d268214c9b070cd55
Ali Rizwan <arizwan@alphabold.com>
2019-11-07 06:59:15 +0000

    Intial Commit

--- src/index.php
+++ src/index.php
@@ -1,17 +1,17 @@
 <?php
-$conn = new mysqli("database", "root", ".sweetpwd.", "my_db");
+$conn = new mysqli("database", "root", "pwd", "my_db");
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT name FROM users";
+$sql = "SELECT * FROM agents";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while ($row = $result->fetch_assoc()) {
-        echo $row['name']."<br>";
+        echo "<br> ID: ". $row["AGENT_CODE"]. "  " ."<br>Name: ". $row["AGENT_NAME"] ."<br>Area: ". $row["WORKING_AREA"] . "<br><br>";
     }
 } else {
     echo "0 results";
